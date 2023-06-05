807449b564b0242b6fa18f7970461e06310d3983
Sachin Shrivastav <43625451+Sachin-251@users.noreply.github.com>
2023-06-06 02:19:38 +0530

    Update send_code.php

--- assets/php/send_code.php
+++ assets/php/send_code.php
@@ -21,8 +21,8 @@ global $mail;
         $mail->isSMTP();                                            //Send using SMTP
         $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'firstfallen.251@gmail.com';                     //SMTP username
-        $mail->Password   = 'dohwjdfhymrcoeob';                               //SMTP password
+        $mail->Username   = '[YOUR EMAIL]';                     //SMTP username
+        $mail->Password   = '[EMAIL PASSWORD]';                               //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
         $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
     
